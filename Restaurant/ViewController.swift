7425da817a4da60ab354b1b9898b3b4b470606dd
Chislaine Cruz <chislaine.cruz@gmail.com>
2019-10-23 19:28:40 -0400

    Initial Commit

---
+++ Restaurant/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Restaurant
+//
+//  Created by Chislaine Cruz on 10/23/19.
+//  Copyright © 2019 Chislaine Cruz. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
