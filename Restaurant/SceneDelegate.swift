dc871ae66deb2185151f87f5f141509b7e6f125b
Chislaine Cruz <chislaine.cruz@gmail.com>
2019-10-26 15:13:07 -0400

    notification badge updates when item is added to order

--- Restaurant/SceneDelegate.swift
+++ Restaurant/SceneDelegate.swift
@@ -11,12 +11,16 @@ import UIKit
 class SceneDelegate: UIResponder, UIWindowSceneDelegate {
 
     var window: UIWindow?
-
+    var orderTabBarItem: UITabBarItem!
 
     func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
         // Use this method to optionally configure and attach the UIWindow `window` to the provided UIWindowScene `scene`.
         // If using a storyboard, the `window` property will automatically be initialized and attached to the scene.
         // This delegate does not imply the connecting scene or session are new (see `application:configurationForConnectingSceneSession` instead).
+        NotificationCenter.default.addObserver(self, selector: #selector(updateOrderBadge), name: MenuController.orderUpdatedNotification, object: nil)
+
+        orderTabBarItem = (self.window?.rootViewController as? UITabBarController)?.viewControllers?[1].tabBarItem
+        
         guard let _ = (scene as? UIWindowScene) else { return }
     }
 
@@ -48,6 +52,9 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
         // to restore the scene back to its current state.
     }
 
-
+    @objc func updateOrderBadge() {
+        orderTabBarItem.badgeValue = String(MenuController.shared.order.menuItems.count)
+    }
+    
 }
 
